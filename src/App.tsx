0f41f4fbe37ed475dc7056b65ebeceef59ba5416
Filip Krunic <filip.krunic@kdssanalytics.com>
2024-08-07 11:38:35 -0700

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
 
-function App() {
+// function NutritionDisplayRow(property: string, value: string) {
+//     return (
+//         <div className="
+//             w-full
+//
+//             flex
+//             flex-col
+//             gap-[10px]
+//
+//             pl-[32px]
+//             pb-[13px]
+//
+//             border-b
+//             ">
+//
+//         </div>
+//     )
+// }
 
+function App() {
   return (
       <div className="flex flex-col justify-center items-center min-h-screen">
-        <h1 className="text-3xl font-bold underline">Hello World!</h1>
+        <h1 className="text-3xl font-outfit font-bold underline">Hello World!</h1>
       </div>
   )
 }
