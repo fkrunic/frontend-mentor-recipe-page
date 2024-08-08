29a3a0dba336a429c0e2a11f39ddfecd35fc44e8
Filip Krunic <filip.krunic@kdssanalytics.com>
2024-08-08 10:29:17 -0700

    feat: bullet

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import {NutritionSection} from "./components/NutritionSection.tsx";
 import {InstructionSection} from "./components/InstructionSection.tsx";
 import {appData, instructionSection} from "./data.ts";
+import {Bullet} from "./components/Bullet.tsx";
 
 function App() {
   return (
       <div className="flex flex-col justify-center items-center min-h-screen">
           <h1 className="text-3xl font-young-serif font-bold underline">Hello World!</h1>
           <div className="flex flex-col gap-[10px] items-center px-[10px] py-[40px]">
+              <Bullet/>
               <InstructionSection {...instructionSection} />
               <NutritionSection title={appData.title} description={appData.description} entries={appData.entries} />
           </div>
---
+++ src/components/Bullet.tsx
@@ -0,0 +1,5 @@
+export const Bullet: React.FC = () => {
+    return (
+        <div className="w-[4px] h-[4px] rounded-full inline-block bg-black"></div>
+    )
+}
