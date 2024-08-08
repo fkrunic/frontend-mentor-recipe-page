2d081bdbe69831c6f960837bbc719662a0d84f77
Filip Krunic <filip.krunic@kdssanalytics.com>
2024-08-08 16:49:51 -0700

    feat: project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,21 @@ export default {
       fontFamily: {
         outfit: ['Outfit', 'sans-serif'],
         "young-serif": ['Young Serif', 'sans-serif']
+      },
+      colors: {
+        stone: {
+          100: 'hsl(30, 54%, 90%)',
+          150: 'hsl(30, 18%, 87%)',
+          600: 'hsl(30, 10%, 34%)',
+          900: 'hsl(24, 5%, 18%)'
+        },
+        brown: {
+          800: 'hsl(14, 45%, 36%)'
+        },
+        rose: {
+          50: 'hsl(330, 100%, 98%)',
+          800: 'hsl(332, 51%, 32%)'
+        }
       }
     },
   },
