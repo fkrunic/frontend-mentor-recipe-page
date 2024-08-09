cb4a94f2d6421a2ac1d149d34ee39ccccc8131e3
Filip Krunic <filip.krunic@kdssanalytics.com>
2024-08-08 17:59:41 -0700

    fix: nutrition-row borders

--- src/components/desktop/NutritionDisplayRow.tsx
+++ src/components/desktop/NutritionDisplayRow.tsx
@@ -3,18 +3,7 @@ import {NutritionDisplayRowProps} from "../common/schema.ts";
 
 export const NutritionDisplayRow: React.FC<NutritionDisplayRowProps> = ({ property, value }) => {
     return (
-        <div className="
-            w-full
-
-            flex
-            flex-col
-            gap-[10px]
-
-            pl-[32px]
-            pb-[13px]
-
-            border-b
-            ">
+        <div className="w-full flex flex-col gap-[10px] pl-[32px] pb-[13px] border-b last:border-b-0">
             <div className="w-full flex flex-row">
                 <div className="w-[305px] font-outfit text-[16px]">{property}</div>
                 <div className="font-outfit font-bold text-[16px] text-brown-800">{value}</div>
--- src/components/mobile/NutritionDisplayRow.tsx
+++ src/components/mobile/NutritionDisplayRow.tsx
@@ -7,7 +7,7 @@ export interface NutritionDisplayRowProps {
 
 export const NutritionDisplayRow: React.FC<NutritionDisplayRowProps> = ({ property, value}) => {
     return (
-        <div className="w-full flex flex-col pl-[32px] pb-[13px] border-b">
+        <div className="w-full flex flex-col pl-[32px] pb-[13px] border-b last:border-b-0">
             <div className="w-full flex flex-row justify-between items-center">
                 <div className="w-full flex flex-row items-center font-outfit text-[16px]">{property}</div>
                 <div className="w-full flex flex-row items-center font-outfit font-bold text-[16px] text-brown-800">{value}</div>
